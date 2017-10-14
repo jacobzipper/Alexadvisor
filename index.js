f2095879ab2a6e5284fc714ef123c26f6e064b1a
User <zipper@jacobzipper.com>
2017-10-14 03:07:21 -0400

    barebones alexadvisor test

--- index.js
+++ index.js
@@ -4,46 +4,50 @@ var _ = require('lodash');
 var Skill = require('alexa-app');
 var PORTFOLIO_MANAGER_SESSION_KEY = 'portfolio_manager';
 var skillService = new Skill.app('PortfolioManager');
-var DatabaseHelper = require('./database_helper');
-var databaseHelper = new DatabaseHelper();
+//var DatabaseHelper = require('./database_helper');
+//var databaseHelper = new DatabaseHelper();
 
 skillService.launch(function(request, response) {
   var prompt = 'Welcome to Alexadvisor! To add stocks, for example say "add 10 AAPL". To remove stocks, for example say "remove 10 AAPL". To list your stocks, say "list my stocks". To get info about your portfolio, say "tell me about my portfolio".';
   response.say(prompt).shouldEndSession(false);
 });
 
-skillService.intent('advanceStepIntent', {
-    'utterances': ['{next|advance|continue}']
+skillService.intent('AddStockIntent', {
+    "slots": [
+      {
+        "name": "numStocks",
+        "type": "AMAZON.NUMBER"
+      },
+      {
+        "name": "ticker",
+        "type": "LIST_OF_COMMON_STOCKS"
+      }
+    ],
+    'utterances': ['add {numStocks} {ticker}']
   },
   function(request, response) {
-    var portfolioManagerHelper = getPortfolioManagerHelperFromRequest(request);
-    portfolioManagerHelper.currentStep++;
-    portfolioManagerIntentFunction(portfolioManagerHelper, request, response);
+    response.say("Added " + request.slot("numStocks") + " shares of " + request.slot("ticker") + ".").shouldEndSession(false);
   }
 );
 
-skillService.intent('cakeBakeIntent', {
-    'utterances': ['{new|start|create|begin|build} {|a|the} cake']
+skillService.intent('RemoveStockIntent', {
+    "slots": [
+      {
+        "name": "numStocks",
+        "type": "AMAZON.NUMBER"
+      },
+      {
+        "name": "ticker",
+        "type": "LIST_OF_COMMON_STOCKS"
+      }
+    ],
+    'utterances': ['remove {numStocks} {ticker}']
   },
   function(request, response) {
-    var portfolioManagerHelper = new PortfolioManagerHelper({});
-    portfolioManagerIntentFunction(portfolioManagerHelper, request, response);
-  }
-  skillService.intent('repeatStepIntent', {
-    'utterances': ['{repeat}']
-  },
-  function(request, response) {
-    var portfolioManagerHelper = getPortfolioManagerHelperFromRequest(request);
-    portfolioManagerIntentFunction(portfolioManagerHelper, request, response);
+    response.say("Removed " + request.slot("numStocks") + " shares of " + request.slot("ticker") + ".").shouldEndSession(false);
   }
 );
-
-skillService.launch(function(request, response) {
-  var prompt = 'Welcome to Cakebaker!' +
-    'To start baking, say bake a cake';
-  response.say(prompt).shouldEndSession(false);
-});
-
+/*
 skillService.intent('loadCakeIntent', {
     'utterances': ['{load|resume} {|a|the} {|last} cake']
   },
@@ -69,6 +73,6 @@ skillService.intent('saveCakeIntent', {
     response.shouldEndSession(true).send();
     return false;
 }
-);
+);*/
 
 module.exports = skillService;
